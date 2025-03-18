a96da1adbca9e5c30bcd421b513de053f9f91146
maffersilva <161639122+maffersilva@users.noreply.github.com>
2025-03-18 15:19:54 -0300

    18\03\2025

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 document.addEventListener('DOMcontentLoaded', function(){
  const aumetaFonteBotao=Document. getElementById('aumentar-fonte')
+ const diminuirFonteBotaoFonteBotaoFonteBotao=Document. getElementById('diminuir-fonte')
 
  let tamanhoAtualFonte=1;
+
  aumetaFonteBotao.addEventListener('click', function(){
     tamanhoAtualFonte += 0.1;
     document.body.style.fontSize=`${tamanhoAtualFonte}rem`
  })
+ 
  diminuirFonteBotao. addEventListener('click', function(){
       tamanhoAtualFonte .=0.1;
     document.body.style.fontSize='${tamanhoAtualFonte}rem'
